c4cf282cbcee1fd4f1998e058c3ee35dac7d4c3c
Aiden Lee <aiden.jihun.lee@gmail.com>
2013-07-18 16:46:29 -0400

    added dead and place ship function.

--- place_ship/main.ino
+++ place_ship/main.ino
@@ -8,12 +8,15 @@ struct player {
   ship ship;
 };
 
-boolean attack (x, y) {
+boolean attack (player, x, y) {
   for (int i = 0, i < 3, i ++) {
-    if (ship.x+i == x && ship.y == y) {}
-      ship.health[i] = false;
+    if (player.ship.x+i == x && player.ship.y == y) {}
+      player.ship.health[i] = false;
       lcd.clear();
       lcd.print("HIT");
+      if (dead(player.ship)) {
+        lcd.print("You win!");
+      };
       return true;
     }
   }
@@ -21,3 +24,15 @@ boolean attack (x, y) {
   return false;
 }
 
+boolean dead (ship) {
+  if (ship.health = [false, false, false]) {
+    return true;
+  }
+  return false;
+}
+
+void place_ship() {
+  if (select pressed) {
+    player1.ship = (.x = x, .y = y, .health = [true,true,true]);
+  };
+}
